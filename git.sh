8c5b3b2916ac56369a5202742625ef41cabf93bc
Yusuke <yusuke@Yusuke.local>
2019-11-24 18:04:36 +0900

    update

---
+++ git.sh
@@ -0,0 +1,15 @@
+echo ""
+echo "*****************************"
+echo "start!! herokuサーバーへ修正を反映します"
+echo "*****************************"
+echo ""
+
+git add .
+git commit -m "update"
+git push heroku master
+
+echo ""
+echo "*****************************"
+echo "complete!! 修正が反映されました"
+echo "*****************************"
+echo ""
