b503ca906e02f7b9d86eaee5ca0dde0a143c30ab
Your Name <you@example.com>
2019-12-06 16:20:49 +0900

    update

--- js/main.js
+++ js/main.js
@@ -147,7 +147,19 @@ $(function() {
 			return false;
 		});
 	});
-
 	// スクロールエフェクト  ==========================
 
+	// Gmap の設定  ==========================
+	var map;
+	function initMap() {
+		var target = document.getElementById('gmap');
+		var empire = { lat: 40.748441, lng: -73.985664 };
+		//Empire State Bldg の緯度（latitude）と経度（longitude）
+		map = new google.maps.Map(target, {
+			center: empire,
+			zoom: 14
+		});
+	}
+	initMap();
+	// Gmap の設定  ==========================
 });
