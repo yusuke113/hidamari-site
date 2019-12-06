e3282e8548816929409cac7025c6910b7fcd8189
Your Name <youremail@example.com>
2019-12-06 17:14:46 +0900

    update

--- git.sh
+++ git.sh
@@ -4,6 +4,8 @@ echo "start!! herokuサーバーへ修正を反映します"
 echo "*****************************"
 echo ""
 
+git config user.name "Your Name"
+git config user.email "youremail@example.com"
 git add -A
 git commit -m "update"
 git push heroku master
