dbbeee8ff444b465916a5496a1897e48cfd65531
Your Name <youremail@example.com>
2019-12-12 16:53:08 +0900

    update

---
+++ js/sp-main.js
@@ -0,0 +1,12 @@
+$(function() {
+	'use strict';
+
+  // Menu表示,非表示  ==========================
+  function MenuActive() {
+    $('#Menu').click(function() {
+      $('.MenuLavel').toggle();
+    });
+  }
+  MenuActive();
+  // Menu表示,非表示  ==========================
+});
